f93ec9d93d4491f37df45928215c1fbf51f5508d
0x00-pl <0x00.pl@gmail.com>
2018-09-20 14:25:52 +0100

    use dirname

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def rename_files(src_path, dst_path, map_func, filter_func):
             rel_path_name = os.path.relpath(os.path.join(base, filename), src_path)
             if filter_func(rel_path_name):
                 dst_path_name = os.path.join(dst_path, map_func(rel_path_name))
-                os.makedirs(os.path.split(dst_path_name)[0], exist_ok=True)
+                os.makedirs(os.path.dirname(dst_path_name), exist_ok=True)
                 os.replace(os.path.join(src_path, rel_path_name), dst_path_name)
 
 
 def rename_base64(name_path):
     dir_name, file_name_ext = os.path.split(name_path)
     file_name, file_ext = os.path.splitext(file_name_ext)
-    file_name_base64 = base64.b64encode(file_name.encode('utf-8')).decode('utf-8')
+    file_name_base64 = base64.b64encode(file_name.encode('utf-8', errors='ignore')).decode('utf-8')
     return os.path.join(dir_name, ''.join([file_name_base64, file_ext]))
 
 
